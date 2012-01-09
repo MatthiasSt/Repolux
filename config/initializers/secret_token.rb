d368baae7a611045a75ddb0af49dec46aa4c8fe0
Matthias <matthias@photocase.com>
2012-01-09 17:09:14 +0100

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Test01::Application.config.secret_token = '2832f3509db08acf458215e8746c5f2d9ec85b26e98cda366c519703ebbdd31f0ae69925b03e95d93ead727105ee4c1f6b293e4424c87fae6f289dd959ef3264'
